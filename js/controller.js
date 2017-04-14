e47e8ad6400beba8411e90cab8e958d84177fc0f
rmcdanielslc <rmcdnaielslc@gmail.com>
2017-04-13 20:04:20 -0600

    group page done

--- js/controller.js
+++ js/controller.js
@@ -1,8 +1,9 @@
-angular.module('designApp').controller('mainController', function($scope){
+var app = angular.module('designApp').controller('mainController', function($scope){
 
  $scope.createUser = function(user) {
    $scope.defaultUser = user;
    $scope.user = {};
+   console.log($scope.defaultUser)
  }
 
   $scope.profiles = function (){
@@ -20,6 +21,68 @@ var hideOption = ['updateEmail', 'updateSettings'];
     })
   };
 
+$scope.createGroup = function (group){
+  $scope.group = {
+    name:'',
+    objective:'',
+    image:''
+  };
+  $scope.groups.push(group);
+    console.log(group,$scope.groups)
+}
+
+$scope.clearFields = function() {
+  $scope.defaultUser.firstName = "";
+  $scope.defaultUser.lastName = "";
+  $scope.defaultUser.email = "";
+  $scope.defaultUser.password = "";
+  $scope.defaultUser.imageUrl = "";
+  $scope.defaultUser.location = "";
+  $scope.defaultUser.bio = "";
+};
+
+$scope.clearFieldsGroup = function() {
+  $scope.group.name = "";
+  $scope.group.objective = "";
+  $scope.group.image = "";
+
+};
+
+$scope.currentPage = 0;
+    $scope.pageSize = 5;
+
+$scope.numberOfPages=function(){
+        return Math.ceil($scope.groups.length/$scope.pageSize);
+    }
+    for (var i=0; i<45; i++) {
+      //  $scope.groups.push("group "+ i)
+    }
+
+app.filter('startFrom', function() {
+    return function(input, start) {
+        start = + start;
+        return input.slice(start);
+    }
+});
+
+$scope.groups = [
+  {
+  name: 'Design',
+  objective: 'We like to create stuff'
+},
+{
+name: 'Photography',
+objective: 'We like to take photos'
+},
+{
+name: 'Work',
+objective: 'Group projects'
+},
+{
+name: 'Random Stuff',
+objective: 'I dont know what to do'
+},
+];
 
 
 
